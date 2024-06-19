5b9676d2cef5b79f002e02aa190c93bc26b7c30a
Moazzam Saleem <moazzam0298@gmail.com>
2024-06-19 06:24:46 +0500

    Add Pagination in userList API and change the response in activeUsers and blockedUsers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,10 @@ import { userListService } from "../services/userService.js";
 
 const userListController = async (req, res) => {
   try {
-    const users = await userListService(req.body);
+    const page = parseInt(req.query.page) || 1; // Default to page 1 if not provided
+    const limit = parseInt(req.query.limit) || 100; // Default to 100 users per page if not provided
+
+    const users = await userListService(page, limit);
     res.status(200).json(users);
   } catch (error) {
     res.status(400).json({ error: error.message });
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,9 +1,31 @@
 import { User } from "../models/userModel.js";
 
-const userListService = async () => {
+const userListService = async (page = 1, limit = 10) => {
   try {
-    const users = await User.find({ isVerified: true });
-    const serializedUsers = users.map((user) => User.serialize(user));
+    // Calculate skip value based on page number and limit
+    const skip = (page - 1) * limit;
+
+    // Query users with pagination and isVerified condition
+    const users = await User.find({ isVerified: true })
+      .skip(skip)
+      .limit(limit)
+      .lean(); // Convert Mongoose documents to plain JavaScript objects for better performance
+
+    // Separate blocked and unblocked users during mapping
+    const serializedUsers = {
+      activeUsers: [],
+      blockedUsers: [],
+    };
+
+    users.forEach((user) => {
+      const serializedUser = User.serialize(user);
+      if (user.blocked) {
+        serializedUsers.blockedUsers.push(serializedUser);
+      } else {
+        serializedUsers.activeUsers.push(serializedUser);
+      }
+    });
+
     return serializedUsers;
   } catch (error) {
     console.error("Error: ", error);
